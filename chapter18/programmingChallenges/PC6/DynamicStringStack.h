df14e1bd866d6dca1da29679eafe3d5c42765902
lybahoang <lybahoang1@gmail.com>
2025-07-09 19:42:45 +0700

    Add the DynamicStringStack class to PC6 chapter 18

---
+++ chapter18/programmingChallenges/PC6/DynamicStringStack.h
@@ -0,0 +1,32 @@
+#ifndef DYNAMIC_STRING_STACK_H
+#define DYNAMIC_STRING_STACK_H
+#include <string>
+using namespace std;
+
+class DynamicStringStack
+{
+private:
+    // Structure for the stack node.
+    struct StackNode
+    {
+        string value;       // The node value.
+        StackNode *next;    // The next node.
+    };
+
+    StackNode *top;         // The top of the stack.
+    int numNodes;           // The number of nodes.
+
+    public:
+    // Constructor.
+    DynamicStringStack()
+        { top = nullptr; }
+    
+    // Stack operations.
+    void push(string);
+    void pop(string &);
+    bool isEmpty() const;
+
+    // Other functions.
+    int getNumNodes() const;
+};
+#endif
