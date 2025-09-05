0436faf0c79ef03628702eb74a0bdbd1b38e57d4
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-05 09:30:09 +0700

    add main.cpp file to pc1 chapter 15

--- chapter15/pc1/main.cpp
+++ chapter15/pc1/main.cpp
@@ -1,3 +1,4 @@
+#include "ProductionWorker.h"
 #include "Employee.h"
 #include <iostream>
 #include <string>
@@ -5,8 +6,17 @@ using namespace std;
 
 int main()
 {
-    Employee employee1("Ly Ba Hoang", "1695172", "25 September 2027");
-    employee1.displayInfo();
+    // Create two objects of Employee and ProductionWorker.
+    Employee *employee1 = new Employee("Ly Ba Hoang", "1695172", "25 September 2027");
+    Employee *employee2 = new ProductionWorker("Jack", "32903", "05 September 2005", 1, 25.5);
+
+    // Display the information of employee1.
+    cout << "Employee #1" << endl;
+    employee1->displayInfo();
+
+    // Display the information of employee2.
+    cout << "\nEmployee #2" << endl;
+    employee2->displayInfo();
 
     return 0;
 }
