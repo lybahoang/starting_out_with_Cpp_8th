8983fc17df5da478788a8541cfac0860aa9d8a1f
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-05 09:05:33 +0700

    add Employee.cpp to pc1 chapter 15

---
+++ chapter15/pc1/main.cpp
@@ -0,0 +1,12 @@
+#include "Employee.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    Employee employee1("Ly Ba Hoang", "1695172", "25 September 2027");
+    employee1.displayInfo();
+
+    return 0;
+}
