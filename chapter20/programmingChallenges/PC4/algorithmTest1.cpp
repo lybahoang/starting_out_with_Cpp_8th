3d2456990d8dfc69c2badf63077ab3d1c19148e7
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-19 09:17:29 +0700

    complete two algorithms for pc 4 chapter 20

---
+++ chapter20/programmingChallenges/PC4/algorithmTest1.cpp
@@ -0,0 +1,118 @@
+/**
+    - The program demonstrates an iterative method to determine the height of a binary tree.
+    - The height of a binary tree (or the maximum depth) is defined as the maximum number of
+    edges in the tree from the root to the deepest node.
+    - The pseudocode of the algorithm:
+        1. Create a queue to store nodes in the tree.
+        2. Create two variables: levelSize (the number of nodes in the current level) and
+           depth (the maximum depth of the tree).
+        3. Enqueue the root into the queue.
+        4. While the queue is not empty, then
+            4.1. Store the number of current nodes in the queue as levelSize.
+            4.2. For each node in the current level (loop from front to rear):
+                4.2.1. Dequeue the front node.
+                4.2.2. Enqueue the node's left child and right child, if it has.
+            4.3. Increment depth.
+        5. Return (depth - 1) as the number of edges will be one less than the number of nodes.
+ */
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class TreeNode 
+{
+public:
+    int value;          // The value of the node.
+    TreeNode *left;     // The node's left child.
+    TreeNode *right;    // The node's right child.
+
+    // Constructor of the class.
+    TreeNode(int data) {
+        value = data;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    // Destructor.
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
+};
+
+int height(const TreeNode * nodePtr)
+{
+    // Variables declaration.
+    int levelSize;      // The number of nodes in the current level.
+    int depth = 0;      // The depth of the tree.
+
+    // Create a queue object to hold the tree nodes.
+    queue<const TreeNode *> nodesArray;
+
+    // Enqueue the root into the queue.
+    nodesArray.push(nodePtr);
+
+    // Enter a loop to process the queue and the tree.
+    while (nodesArray.empty() == false)
+    {
+        // Store the current number of nodes in levelSize.
+        levelSize = nodesArray.size();
+
+        // Enqueue children of current nodes into the queue.
+        for (int index = 0; index < levelSize; index++)
+        {
+            // Take a reference to the front node.
+            const TreeNode *currentNode = nodesArray.front();
+
+            // Pop the front node off the queue.
+            nodesArray.pop();
+
+            // Enqueue the front node's left and right children if exist.
+            if (currentNode->left)
+                nodesArray.push(currentNode->left);
+            if (currentNode->right)
+                nodesArray.push(currentNode->right);
+        }
+
+        // Increment depth variable.
+        depth++;
+    }
+
+    // Return the height of the tree.
+    return (depth - 1);
+}
+
+int main()
+{
+    /** Demo input:
+                12
+               /  \
+              8    18
+             / \
+            5   11
+     */
+
+    // Declare a root of the tree.
+    TreeNode *root = new TreeNode(12);
+
+    // Declare children of the root.
+    TreeNode *children1_L = new TreeNode(8);
+    TreeNode *children1_R = new TreeNode(18);
+    root->left = children1_L;
+    root->right = children1_R;
+
+    // Declare children for the left node.
+    TreeNode *children2_L = new TreeNode(5);
+    TreeNode *children2_R = new TreeNode(11);
+    children1_L->left = children2_L;
+    children1_L->right = children2_R;
+
+    // Display the height of the tree.
+    cout << "Height: " << height(root) << endl;
+
+    // Delete dynamically allocated memomry.
+    delete root;
+
+    return 0;
+}
---
+++ chapter20/programmingChallenges/PC4/algorithmTest2.cpp
@@ -0,0 +1,96 @@
+/**
+    - The program demonstrates a recursive method to determine the height of a binary tree.
+    - The height (or the maximum depth) of a binary tree is the maximum number of edges in
+    the tree from the root to the deepest node.
+    - The ideas of the algorithm:
+        1. Base case: If the tree is empty, then retun -1.
+        2. Recursive step:
+            2.1. Get the height of the left subtree recursively.
+            2.2. Get the height of the right subtree recursively.
+            2.3. Compute the maximum of the heights of the left and the right subtrees and
+                 add 1 to it for the current node.
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/**
+    The TreeNode class represents a node in the tree.
+ */
+class TreeNode
+{
+public:
+    int value;          // The value of the node.
+    TreeNode *left;     // The left child of the node.
+    TreeNode *right;    // The right child of the node.
+
+    // Constructor.
+    TreeNode(int data) {
+        value = data;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    // Destructor.
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
+};
+
+/**
+    The height function determines the height of the tree recursively and
+    returns that value.
+    @param nodePtr The root of a subtree.
+    @return The height of the tree.
+ */
+int height(const TreeNode *nodePtr)
+{
+    // The base case: If the tree is empty, then return -1.
+    if (nodePtr == nullptr)
+        return -1;
+
+    // The recursive step.
+    // Get the height of the left subtree recursively.
+    int leftHeight = height(nodePtr->left);
+
+    // Get the height of the right subtree recursively.
+    int rightHeight = height(nodePtr->right);
+
+    // Return the height of the tree.
+    return max(leftHeight, rightHeight) + 1;
+}
+
+int main()
+{
+    /** Demo input:
+                12
+               /  \
+              8    18
+             / \
+            5   11
+     */
+
+    // Create the root of the tree.
+    TreeNode *root = new TreeNode(12);
+
+    // Create the children of the root.
+    TreeNode *rootLeftChild = new TreeNode(8);
+    TreeNode *rootRightChild = new TreeNode(18);
+    root->left = rootLeftChild;
+    root->right = rootRightChild;
+
+    // Create the children of the left node.
+    TreeNode *child2_1_L = new TreeNode(5);
+    TreeNode *child2_1_R = new TreeNode(11);
+    rootLeftChild->left = child2_1_L;
+    rootLeftChild->right = child2_1_R;
+
+    // Display the height of the tree.
+    cout << "Height: " << height(root) << endl;
+
+    // Delete dynamically allocated memomry.
+    delete root;
+
+    return 0;
+}
